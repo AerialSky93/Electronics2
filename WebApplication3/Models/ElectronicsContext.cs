90867f0bf55eed81536db189264d7d726263fab6
Ritwik Dutta <rdutta23@yahoo.com>
2018-07-07 23:35:21 -0700

    fixed untracked files

---
+++ WebApplication3/Models/ElectronicsContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication3.Models
+{
+    public partial class ElectronicsContext : DbContext
+    {
+        public ElectronicsContext()
+        {
+        }
+
+        public ElectronicsContext(DbContextOptions<ElectronicsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+  
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductDescription)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
