0936fd1a127f5bd721c4eff61f80323f0a6d3f60
Ritwik Dutta <rdutta23@yahoo.com>
2018-09-24 20:30:43 -0700

    aligned navigation bar

--- WebApplication3/Models/Product.cs
+++ WebApplication3/Models/Product.cs
@@ -10,5 +10,6 @@ namespace ElectronicsStore.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
+        public string ImageLocation { get; set; }
     }
 }
--- WebApplication3/Models/SeedData.cs
+++ WebApplication3/Models/SeedData.cs
@@ -29,17 +29,20 @@ namespace ElectronicsStore.Models
                  new Product
                  {
                      ProductName = "Samsung T3 Portable SSD - 500GB",
-                     ProductDescription = "Superfast Read-Write Speeds of up to 450 MB/s; Shock Resistant & Secure Encryption"
+                     ProductDescription = "Superfast Read-Write Speeds of up to 450 MB/s; Shock Resistant & Secure Encryption",
+                     ImageLocation = "Product1.jpg"
                  },
                  new Product
                  {
                      ProductName = "Acer R240HY bidx 23.8-Inch IPS HDMI DVI VGA (1920 x 1080) Widescreen Monitor",
-                     ProductDescription = "The Acer R240HY 23.8” IPS display shows every detail clearly and vivid without color difference from any viewing angle. Its zero frame design puts no boundary on your visual enjoyment while the brushed hairline finish stand matches any environment. It also supports VGA, DVI & HDMI inputs so you can easily power and extend the enjoyment from your smartphone or tablet on Full HD display."
+                     ProductDescription = "The Acer R240HY 23.8” IPS display shows every detail clearly and vivid without color difference from any viewing angle. Its zero frame design puts no boundary on your visual enjoyment while the brushed hairline finish stand matches any environment. It also supports VGA, DVI & HDMI inputs so you can easily power and extend the enjoyment from your smartphone or tablet on Full HD display.",
+                     ImageLocation = "Product2.jpg"
                  },
                  new Product
                  {
                      ProductName = "Panasonic On-Ear Stereo Headphones RP-HT21 ",
-                     ProductDescription = "Lightweight, open-air design on-ear headphones weigh just 1.2 oz. (without cable). Connectivity Technology: Wired;30mm large neodymium drivers deliver rich powerful bass and natural treble."
+                     ProductDescription = "Lightweight, open-air design on-ear headphones weigh just 1.2 oz. (without cable). Connectivity Technology: Wired;30mm large neodymium drivers deliver rich powerful bass and natural treble.",
+                     ImageLocation = "Product3.jpg"
                  });
                  context.SaveChanges();
             }
