4cc0d257254df7ca02f135a6bf51fa470f27975d
Ritwik Dutta <rdutta23@yahoo.com>
2018-10-14 02:18:56 -0700

    removed comments

--- WebApplication3/Controllers/ShoppingCartController.cs
+++ WebApplication3/Controllers/ShoppingCartController.cs
@@ -7,36 +7,36 @@ namespace ElectronicsStore.Controllers
 
     public class ShoppingCartController : Controller
     {
-        private IProductRepository<Product> productRepository;
-        private ShoppingCartRepository shoppingCartRepository;
+        private IProductRepository<Product> _productRepository;
+        private ShoppingCartRepository _shoppingCartRepository;
 
 
         public ShoppingCartController(IProductRepository<Product> productRepository, ShoppingCartRepository shoppingCartRepository)
         {
-            productRepository = productRepository;
-            shoppingCartRepository = shoppingCartRepository;
+            _productRepository = productRepository;
+            _shoppingCartRepository = shoppingCartRepository;
         }
 
         public ViewResult Index(string returnUrl)
         {
             return View(new ShoppingCartViewModel
             {
-                ShoppingCart = shoppingCartRepository.GetShoppingCart(),
+                ShoppingCart = _shoppingCartRepository.GetShoppingCart(),
                 ReturnUrl = returnUrl
             });
         }
 
         public RedirectToActionResult AddToCart(int productid, string returnUrl)
         {
-            Product product = productRepository.GetById(productid);
-            shoppingCartRepository.AddItem(product, 1);
+            Product product = _productRepository.GetById(productid);
+            _shoppingCartRepository.AddItem(product, 1);
 
             return RedirectToAction("Index", new { returnUrl });
         }
 
         public RedirectToActionResult RemoveFromCart(int cartlineid, string returnUrl)
         {
-            shoppingCartRepository.RemoveItem(cartlineid);
+            _shoppingCartRepository.RemoveItem(cartlineid);
 
             return RedirectToAction("Index", new { returnUrl });
         }
