8b532837601fadee29bb3fbdfa3695309098121e
Ritwik Dutta <rdutta23@yahoo.com>
2019-02-10 21:44:48 -0800

    removed unrequired items

--- WebApplication3/Models/Info.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ElectronicsStore.Models
-{
-    public class Info
-    {
-        //Scaffold-DbContext "Server=localhost;Database=Electronics;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -force
-    }
-}
