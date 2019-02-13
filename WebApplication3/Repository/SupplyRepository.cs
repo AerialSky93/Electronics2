87ca8e4d7dd48bedb319a1437d171a980858fbef
Ritwik Dutta <rdutta23@yahoo.com>
2019-02-12 22:54:59 -0800

    modified supply repository

--- WebApplication3/Repository/SupplyRepository.cs
+++ WebApplication3/Repository/SupplyRepository.cs
@@ -21,7 +21,6 @@ namespace ElectronicsStore.Repository
             _context = context;
             productrepository = new ProductRepository(_context);
             vendorrepository = new VendorRepository(_context);
-
         }
 
         public IEnumerable<Supply> GetAllSupply()
@@ -29,36 +28,11 @@ namespace ElectronicsStore.Repository
             return _context.Supply.ToList();
         }
 
-        //public async void ProcessFiles(string filePath)
-        //{
-        //    //StreamReader reader = File.OpenText("filename.txt");
-
-        //    //List<VendorSupply> values = File.ReadAllLines(@"C:\Users\Ritwik\Desktop\filenametest.txt")
-        //    //                                .Select(v => VendorSupply.FromCsv(v))
-        //    //                                .ToList();
-
-
-        //    List<VendorSupply> values = File.ReadAllLines(filePath)
-        //                                    .Select(v => ParseVendorSupply.FromCsv(v))
-        //                                    .ToList();
-
-        //    foreach (VendorSupply i in values)
-        //    {
-        //        var product = productrepository.GetById(i.ProductId);
-        //        var vendor = vendorrepository.GetById(i.VendorId);
-        //        _context.Supply.Add(new Supply { Vendor = vendor, Product = product, SupplyQuantity = i.Quantity });
-        //        await _context.SaveChangesAsync();
-        //    }
-
-        //}
-
         public IQueryable<Supply> Supply => _context.Supply;
 
         public Product GetById(int SupplyId)
         {
-
             return _context.Product.Find(SupplyId);
-
         }
     }
 }
